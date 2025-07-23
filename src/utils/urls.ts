d726192f6a3dd73b5d508f385c352f4c12423853
Ma'ruf Shabonov <shabonov.m@bnpz.local>
2025-07-23 16:45:28 +0500

    delete

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://127.0.0.1:8005/api/v1"
-export const BASE_IMAGE_URL = "http://127.0.0.1:8005"
-//  export const BASE_URL = "http://192.168.101.6:8005/api/v1"
-//  export const BASE_IMAGE_URL = "http://192.168.101.6:8005"
+// export const BASE_URL = "http://127.0.0.1:8005/api/v1"
+// export const BASE_IMAGE_URL = "http://127.0.0.1:8005"
+ export const BASE_URL = "http://192.168.101.6:8005/api/v1"
+ export const BASE_IMAGE_URL = "http://192.168.101.6:8005"
 //export const BASE_URL = "https://inv.bnpz.uz/api/v1"
 //export const BASE_IMAGE_URL = "https://inv.bnpz.uz"
 
